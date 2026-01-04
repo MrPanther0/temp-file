90b2cbf48a1178a0668775d9db0417ade662eaf5
MrPanther0 <109415681+MrPanther0@users.noreply.github.com>
2026-01-04 19:12:21 +0530

    Update files.js

--- api/files.js
+++ api/files.js
@@ -6,7 +6,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     // Fix the storage bucket name here:
-    storageBucket: 'gs://temp-file-29f4a.firebasestorage.app'  // <-- correct bucket name format
+    storageBucket: 'temp-file-29f4a.firebasestorage.app'  // <-- correct bucket name format
   });
 }
 
@@ -106,4 +106,5 @@ export default async function handler(req, res) {
     res.status(500).send('Failed to list files');
   }
 }
+
 
