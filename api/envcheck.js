45166f01ba8de5d8e20b49c046432d296a8459bd
MrPanther0 <109415681+MrPanther0@users.noreply.github.com>
2026-01-04 18:36:55 +0530

    Update and rename test.js to envcheck.js

--- api/envcheck.js
+++ api/envcheck.js
@@ -1,8 +1,11 @@
 export default function handler(req, res) {
   try {
     const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-    if (!serviceAccount) throw new Error("No service account found");
-    res.status(200).json({ message: "Env loaded successfully", project_id: serviceAccount.project_id });
+    res.status(200).json({
+      project_id: serviceAccount.project_id,
+      bucket: 'gs://temp-file-29f4a.firebasestorage.app',
+      envVarExists: !!process.env.FIREBASE_SERVICE_ACCOUNT
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
