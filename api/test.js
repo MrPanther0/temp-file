7ee22722d614ceebd5d81216d1bb8d6f37779a88
MrPanther0 <109415681+MrPanther0@users.noreply.github.com>
2026-01-04 18:33:31 +0530

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,1 +1,9 @@
-
+export default function handler(req, res) {
+  try {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    if (!serviceAccount) throw new Error("No service account found");
+    res.status(200).json({ message: "Env loaded successfully", project_id: serviceAccount.project_id });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
